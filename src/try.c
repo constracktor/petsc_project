b4865cb159292f2b5bbe5e8655b77d601325a3ff
Alexander Strack <strack@MacbookPro-Alexander.fritz.box>
2022-07-11 15:25:31 +0200

    compute regressor vector 4

--- src/try.c
+++ src/try.c
@@ -153,6 +153,7 @@ int main(int argc,char **args)
     for (j = 1; j < n_training; j++)
     {
       // compute regressor vectors
+      PetscScalar u_i[n_regressors];
       u_i = compute_regressor_vector(i, n_regressors, training_input);
       //u_j = 1;
 
