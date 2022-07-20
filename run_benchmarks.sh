82e83d73e39f1be59c28cf2101bea0467e09faf6
constracktor <strack.as@icloud.com>
2022-07-20 15:29:50 +0200

    benchmark script

---
+++ run_benchmarks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Compile Code
+cd && cd petsc_project/src && make petsc_cholesky && export OMPI_MCA_btl_vader_single_copy_mechanism=none
+# Run cores_script
+START=1
+END=6
+STEP=1
+N_TRAIN=1000
+N_TEST=1000
+N_REG=100
+cd && cd petsc_project/scripts && chmod +x cores_script.sh && ./cores_script.sh $START $END $STEP $N_TRAIN $N_TEST $N_REG | tee -a cores_result.txt
+# Run data_script
