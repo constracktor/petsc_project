a582726fddd4018ed5f64c68a964a458761056e6
constracktor <strack.as@icloud.com>
2022-07-20 15:44:09 +0200

    add data script

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -10,3 +10,10 @@ N_TEST=1000
 N_REG=100
 cd && cd petsc_project/scripts && chmod +x cores_script.sh && ./cores_script.sh $START $END $STEP $N_TRAIN $N_TEST $N_REG | tee -a cores_result.txt
 # Run data_script
+START=1000
+END=5000
+STEP=1000
+N_CORES=6
+N_TEST=1000
+N_REG=100
+cd && cd petsc_project/scripts && chmod +x data_script.sh && ./data_script.sh $START $END $STEP $N_CORES $N_TEST $N_REG | tee -a data_result.txt
--- scripts/data_script.sh
+++ scripts/data_script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# since Bash v4
-for i in {5000..100000..5000}
+touch data_result.txt && echo 'Cores;Time;Error;N_train;N_test;N_regressor;' >> data_result.txt
+cd ../src
+START=$1
+END=$2
+STEP=$3
+N_CORES=$4
+N_TEST=$5
+N_REG=$6
+for (( i=$START; i<= $END; i=i+$STEP ))
 do
-     time mpirun -n 6 ../src/petsc_cholesky -n_train $i -n_test 5000
+     mpirun -n $N_CORES ../src/petsc_cholesky -n_train $i -n_test $N_TEST -n_regressors $N_REG
 done
